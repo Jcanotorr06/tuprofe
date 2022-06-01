1b3177d95415414e6e2175570480ea961224b788
Joseph Cano <joseph@paguelofacil.com>
2022-06-01 04:40:01 +0000

    update: start design of landing page and add test route

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1>{{ $t("welcome")}}</h1>
+  <div class="p-5">
+    <div class="surface w-100 rounded-lg relative">
+      <div class="w-100 surface shadow-lg rounded-lg px-32 py-7 flex justify-between">
+          <div class="flex h-100 items-center">
+              <div class="bg-gray-400 flex justify-center items-center p-4 rounded-lg">
+                  <b-icon icon="book-half"/>
+              </div>
+              <div class="">
+                  <div>
+                    <h1>HELLO</h1>
+                  </div>
+                  <div>
+                    <p>FUCK</p>
+                  </div>
+              </div>
+          </div>
+          <div>HELLO</div>
+          <div>HELLO</div>
+      </div>
+      <div>
+        <div class="h-96"></div>
+      </div>
+    </div>
   </div>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+       <span>TEST</span>
+    </div>
+</template>
